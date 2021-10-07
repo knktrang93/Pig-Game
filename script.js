8952e21c116f58d0e41bb130a88ff6bc2d7a8811
knktrang93 <87623875+knktrang93@users.noreply.github.com>
2021-10-07 15:36:38 -0700

    added files

---
+++ script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// Selecting elements
+const score0El = document.querySelector("#score--0");
+const score1El = document.querySelector("#score--1");
+const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+
+// Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add("hidden");
+
+// Rolling dice functionality
+btnRoll.addEventListener("click", function () {
+  // Generating random dice roll
+  const dice = 
+  // Display dice
+  // Check for roll 1, true: switch to next player
+});
